76fac126d6da64559575aee2deb9c9d1add14405
chinawin <chinawin47@gmail.com>
2026-03-25 12:35:00 +0700

    submit assign

--- ex-1.js
+++ ex-1.js
@@ -3,24 +3,34 @@ const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
 function addSalary5000(previousSalary) {
   // Start coding here
+  return previousSalary + 5000;
 }
 
 function forEach(array, operation) {
   // Start coding here
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(operation(array[i]));
+  }
+  return result;
 }
 
 // Using `forEach` function here
 
-let newEmployeeSalaries;
+let newEmployeeSalaries = forEach(employeeSalaries, addSalary5000);
 
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+console.log(newEmployeeSalaries);
+// [25005, 45000, 37000, 19500, 349000]
 
 /*
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+  
+function addSalary5000 คือ Callback Function เพราะถูกนำไปใช้ในอีก Function หนึ่งคือ forEach
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
 
+function forEach คือ Higher-Order Function เพราะใน forEach มี operation เป็นตัวรับ Function อื่นมา Execute ข้างใน
 ====================================
 */
--- ex-2.js
+++ ex-2.js
@@ -5,17 +5,24 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 function checkGradeOver70(score) {
   // Start coding here
+  return score >= 70 ? true : false;
 }
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let result = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i])) {
+      result++;
+    }
+  }
+  return result >= 5;
 }
-
 // Using `atLeastFive` function here
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
-
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkGradeOver70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkGradeOver70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkGradeOver70);
 console.log(scoreRoom1Result); //true
 console.log(scoreRoom2Result); //false
 console.log(scoreRoom3Result); //false
@@ -24,8 +31,9 @@ console.log(scoreRoom3Result); //false
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+function checkGradeOver70
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
-
+function atLeastFive
 ====================================
 */
